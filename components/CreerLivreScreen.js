f823bc0025b254c7e335508ef2a0e5b6ae047609
Thom's <thomcooper04@gmail.com>
2023-06-06 12:27:31 +0200

    Amélioration du style form ajout de livre

--- components/CreerLivreScreen.js
+++ components/CreerLivreScreen.js
@@ -1,42 +1,65 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-import { LIVRES } from '../models/data.js';
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Picker,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { v4 as uuidv4 } from "uuid";
+import { LIVRES, CATEGORIES } from "../models/data.js";
 
 const CreerLivreScreen = ({ navigation }) => {
-  const [titre, setTitre] = useState('');
-  const [description, setDescription] = useState('');
+  const [titre, setTitre] = useState("");
+  const [description, setDescription] = useState("");
+  const [categorieId, setCategorieId] = useState("");
+
+  const [livres, setLivres] = useState(LIVRES);
 
   const ajouterLivre = () => {
     const nouveauLivre = {
-      id: LIVRES.length + 1, // Générer un nouvel ID unique pour le livre
+      id: uuidv4(),
       titre,
       description,
+      categorieId,
     };
 
-    // Ajouter le nouveau livre à la constante LIVRES
-    LIVRES.push(nouveauLivre);
-    console.log(nouveauLivre);
-
-    // Rediriger vers la liste des livres
-    navigation.navigate('Livres');
+    setLivres([...livres, nouveauLivre]);
+    navigation.goBack();
   };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Ajouter un livre</Text>
       <TextInput
         style={styles.input}
-        placeholder="Titre du livre"
+        placeholder="Titre"
         value={titre}
         onChangeText={setTitre}
       />
       <TextInput
         style={styles.input}
-        placeholder="Description du livre"
+        placeholder="Description"
         value={description}
         onChangeText={setDescription}
-        multiline
       />
-      <Button title="Ajouter le livre" onPress={ajouterLivre} />
+      <Picker
+        selectedValue={categorieId}
+        onValueChange={(value) => setCategorieId(value)}
+        style={styles.picker}
+      >
+        {CATEGORIES.map((categorie) => (
+          <Picker.Item
+            key={categorie.id}
+            label={categorie.genre}
+            value={categorie.id}
+          />
+        ))}
+      </Picker>
+      <TouchableOpacity style={styles.button} onPress={ajouterLivre}>
+        <Text style={styles.buttonText}>Ajouter un livre</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -44,18 +67,41 @@ const CreerLivreScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingHorizontal: 20,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
   input: {
-    width: '100%',
+    width: "100%",
     height: 40,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
-    marginBottom: 10,
+    marginBottom: 20,
     paddingHorizontal: 10,
+  },
+  picker: {
+    width: "100%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#f5a442",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
 
